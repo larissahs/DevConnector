a47fffdf13d91f3e1dbba9fd07143bb2a8ad68ba
Larissa Hoshiko Suganuma <lala94032@gmail.com>
2019-06-28 17:13:45 -0700

    COMMIT

--- server.js
+++ server.js
@@ -3,9 +3,13 @@ const mongoose = require('mongoose');
 const users = require('./routes/api/users');
 const profile = require ('./routes/api/profile');
 const posts = require('./routes/api/posts');
-const app =  express();
 const bodyParser = require('body-parser');
 const passport = require('passport');
+
+
+const app =  express();
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 //Db config
 const db = require ('./config/keys').mongoURI;
