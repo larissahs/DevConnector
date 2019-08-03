1679761f2b27a800ba62d6b891732c78e7cffc3a
Larissa Hoshiko Suganuma <lala94032@gmail.com>
2019-08-03 11:36:00 -0700

    commit

--- server.js
+++ server.js
@@ -1,34 +1,46 @@
-const express =  require('express');
+const express = require('express');
 const mongoose = require('mongoose');
-const users = require('./routes/api/users');
-const profile = require ('./routes/api/profile');
-const posts = require('./routes/api/posts');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+const path = require('path');
+const app = express();
 
-
-const app =  express();
+//Body parser middleware
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+
 //Db config
-const db = require ('./config/keys').mongoURI;
-mongoose.connect(db).then(() => console.log('MongoDb connected')).catch(err => console.log(err));
+const db = require('./config/keys').mongoURI;
+
+//Connect to MongoDb
+mongoose
+.connect(db)
+.then(() => console.log('MongoDb connected.'))
+.catch(err => console.log(err));
 
 //passport middleware
 app.use(passport.initialize());
 
-//passport config
+//Passport config
 require('./config/passport')(passport);
 
-app.use(bodyParser.urlencoded({extended:false}));
-app.use(bodyParser.json());
-
-app.get('/', (req, res) => res.send('Hello'));
-
+//Use routes
 app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
- 
-const port = 5004;
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+if (process.env.NODE_ENV === 'production'){
+    // set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) =>{
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
+const port = process.env.PORT || 6010;
+app.listen(port, ()=> console.log(`server is running on port ${port}`));
